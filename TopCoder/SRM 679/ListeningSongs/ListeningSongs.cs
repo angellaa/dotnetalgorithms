9bc4dc11128188448b227cb9578415edab753975
Andrea Angella <angella.andrea@gmail.com>
2016-02-12 13:50:11 +0000

    ListeningSongs implementation

---
+++ TopCoder/SRM 679/ListeningSongs/ListeningSongs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+public class ListeningSongs
+{
+    public int Listen(int[] durations1, int[] durations2, int minutes, int T)
+    {
+        Array.Sort(durations1);
+        Array.Sort(durations2);
+
+        var maxTime = minutes * 60;
+
+        if (durations1.Length < T || durations2.Length < T)
+        {
+            return -1;
+        }
+
+        var time = durations1.Take(T).Sum() + durations2.Take(T).Sum();
+
+        if (time > maxTime)
+        {
+            return -1;
+        }
+
+        var durations = durations1.Skip(T).Concat(durations2.Skip(T)).ToArray();
+        Array.Sort(durations);
+
+        var listenedSongs = 2 * T;
+
+        foreach (var duration in durations)
+        {
+            time += duration;
+            if (time > maxTime)
+                break;
+            listenedSongs++;
+        }
+
+        return listenedSongs;
+    }
+}
