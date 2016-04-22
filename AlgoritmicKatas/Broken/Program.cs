c9b2ba985b7d80942dfb8e2ba00f177fbd875022
Andrea Angella <andrea.angella@red-gate.com>
2016-04-22 14:40:49 +0100

    Initial implementation that run out of time

---
+++ AlgoritmicKatas/Broken/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Broken
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Calculate(5, "aaaaaaaaaaaaaa"));
+            Console.WriteLine();
+            Console.WriteLine(Calculate(1, "Mississippi"));
+            Console.WriteLine();
+            Console.ReadKey();
+
+            for (;;)
+            {
+                var m = int.Parse(Console.ReadLine() ?? "0");
+                if (m == 0)
+                {
+                    return;
+                }
+
+                var text = Console.ReadLine();
+
+                Console.WriteLine(Calculate(m, text));
+            }
+        }
+        
+        private static int Calculate(int m, string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return 0;
+            }
+
+            var start = 0;
+            var nextStart = -1;
+            var set = new HashSet<char>();
+            var max = 0;
+
+            for (var i = 0; i < text.Length; i++)
+            {
+                var c = text[i];
+
+                if (set.Contains(c))
+                {
+                    continue;
+                }
+
+                if (set.Count == 1)
+                {
+                    nextStart = i;
+                }
+
+                if (set.Count == m)
+                {
+                    max = Math.Max(max, i - start);
+                    set.Clear();
+                    start = nextStart;
+                    i = start - 1;
+                    continue;
+                }
+
+                set.Add(c);
+            }
+
+            if (max == 0)
+            {
+                return text.Length;
+            }
+
+            return max;
+        }
+    }
+}
