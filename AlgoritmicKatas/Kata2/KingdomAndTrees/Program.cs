b8a8b8effea3a13f03ac02d1980ed50c61d17773
Andrea Angella <andrea.angella@red-gate.com>
2016-06-02 08:10:11 +0100

    Implement solution for Kingdom and trees

---
+++ AlgoritmicKatas/Kata2/KingdomAndTrees/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+
+class KingdomAndTrees
+{
+    public int MinLevel(int[] heights)
+    {
+        var left = 0;
+        var right = 1000000000;
+        var minX = -1;
+
+        while (left <= right)
+        {
+            var x = (left + right) / 2;
+
+            if (CanOrderTrees(heights, x))
+            {
+                minX = x;
+                right = x - 1;
+            }
+            else
+            {
+                left = x + 1;
+            }
+        }
+
+        return minX;
+    }
+
+    private bool CanOrderTrees(int[] heights, int x)
+    {
+        var previousHeight = 0;
+
+        foreach (var height in heights)
+        {
+            if (height + x <= previousHeight)
+            {
+                return false;
+            }
+
+            previousHeight = Math.Max(previousHeight + 1, height - x);
+        }
+
+        return true;
+    }
+}
