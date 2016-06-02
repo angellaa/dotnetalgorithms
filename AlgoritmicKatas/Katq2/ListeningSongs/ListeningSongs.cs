52514944e6635fafb02ef018ff09a0c45a58cc09
Andrea Angella <andrea.angella@red-gate.com>
2016-06-02 07:26:38 +0100

    Add Listening songs

---
+++ AlgoritmicKatas/Katq2/ListeningSongs/ListeningSongs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+public class ListeningSongs
+{
+    public int Listen(int[] durations1, int[] durations2, int minutes, int T)
+    {
+        if (durations1.Length < T || durations2.Length < T)
+        {
+            return -1;
+        }
+
+        Array.Sort(durations1);
+        Array.Sort(durations2);
+
+        var time = durations1.Take(T).Sum() + durations2.Take(T).Sum();
+        var maxTime = minutes * 60;
+
+        if (time > maxTime)
+        {
+            return -1;
+        }
+
+        var durations = durations1.Skip(T).Concat(durations2.Skip(T)).ToArray();
+        Array.Sort(durations);
+
+        var listenedSongs = 2 * T;
+
+        foreach (var duration in durations)
+        {
+            time += duration;
+            if (time > maxTime)
+                break;
+            listenedSongs++;
+        }
+
+        return listenedSongs;
+    }
+}
